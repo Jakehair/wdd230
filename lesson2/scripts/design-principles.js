263a25ea1a8780968e9cb1013c40a0bea2e73767
Jakehair <jahuckob@gmail.com>
2023-01-16 14:13:48 -0700

    design principles website. Finished media queries

---
+++ lesson2/scripts/design-principles.js
@@ -0,0 +1,10 @@
+let year = {year: 'numeric'};
+const copyrightDate = new Intl.DateTimeFormat('en-US', year).format(new Date());
+
+document.querySelector("#copyright").textContent = `©️${copyrightDate}`;
+
+let options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+const formattedDate = new Intl.DateTimeFormat('en-US', options).format(new Date());
+
+document.querySelector("#last-updated").textContent = formattedDate;
+
